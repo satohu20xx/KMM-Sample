f757a9d04300b01dd543f67b3a3ebaf3913b227e
Shinichiro Sato <satohu20xx@gmail.com>
2024-04-26 15:22:18 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,37 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    `maven-publish`
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+}
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://github.com/satohu20xx/KMM-Sample")
+            credentials {
+                username =
+                    "satohu20xx" // project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password =
+                    "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" // project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            groupId = "com.choilabo"
+            artifactId = "kmmsampelibrary"
+            version = "0.0.1"
+//            from(components["java"])
+//            pom {
+//                name = "Satohu20xx KMM Sample"
+//                description = "sample"
+//                url = "https://github.com/satohu20xx/KMM-Sample"
+//            }
+        }
+    }
 }
